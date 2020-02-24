37b3c04dd5673208bc6513cbeb512f099f0191f4
Joey <joeyskuo@gmail.com>
2020-02-23 19:29:30 -0800

    graph subscribed to changes in db

--- graph.js
+++ graph.js
@@ -23,4 +23,34 @@ const pie = d3.pie()
 
 const arcPath = d3.arc()
                     .outerRadius(dimensions.radius)              
-                    .innerRadius(dimensions.radius/2);              
+                    .innerRadius(dimensions.radius/2);      
+                    
+const update = (data) => console.log(data);
+
+var data = [];
+
+db.collection('expenses').onSnapshot(res => {
+
+    res.docChanges().forEach(change => {
+
+        const doc = {...change.doc.data(), id: change.doc.id };
+
+        switch(change.type) {
+            case 'added':
+                data.push(doc);
+                break;
+            case 'modified':
+                const index = data.findIndex(item => item.id == doc.id);
+                data[index] = doc;
+                break;
+            case 'removed':
+                data = data.filter(item => item.id != doc.id);
+                break;
+            default:
+                break;
+        }
+
+    });
+
+    update(data);
+});                    
